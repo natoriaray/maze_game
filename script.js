f910ec968e7a257ee78287a7664fb926f2bf9595
Natoria Ray <natoriaray@gmail.com>
2019-04-21 23:46:02 -0500

    created the modules for my project

---
+++ script.js
@@ -0,0 +1,22 @@
+// DATA CONTROLLER
+var dataController = (function() {
+
+
+
+
+})();
+
+// UI CONTROLLER
+var UIController = (function() {
+
+
+
+})();
+
+// GLOBAL APP CONTROLLER
+var controller = (function(dataCtrl, UICtrl) {
+
+
+
+
+})(dataController, UIController);
