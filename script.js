e9fa86df322bb9ccf9689ae68319ace00943b132
Natoria Ray <natoriaray@gmail.com>
2019-04-22 21:07:43 -0500

    made function for drawing the maze, rectangle, and circle.

--- script.js
+++ script.js
@@ -4,11 +4,43 @@ var dataController = (function() {
 
 
 
+
+
 })();
 
 // UI CONTROLLER
 var UIController = (function() {
 
+  var canvas = document.getElementById('mazecanvas');
+  var context = canvas.getContext('2d'); //returns methods/properties for drawing on the canvas
+  var img = new Image();
+
+  return {
+    drawMazeAndShapes: function() {
+      img.src = '../img/maze.gif';
+      img.onload = function() {
+      // 1. Draw maze image to canvas
+        context.drawImage(img, 0, 0);
+        UIController.drawRect();
+        UIController.drawCir();
+      }
+
+    },
+
+    drawRect: function() {
+      context.beginPath();
+      context.rect(24, 112, 15, 15);
+      context.fillStyle = 'blue';
+      context.fill();
+    },
+
+    drawCir: function() {
+      context.beginPath();
+      context.arc(472, 32, 8, 0, 2 * Math.PI, false);
+      context.fillStyle = '#87FF45';
+      context.fill();
+    }
+  };
 
 
 })();
@@ -16,7 +48,17 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(dataCtrl, UICtrl) {
 
+  var loadMaze = function() {
+    UICtrl.drawMazeAndShapes();
+  }
 
+  return {
+    init: function () {
+      loadMaze();
+    }
+  }
 
 
 })(dataController, UIController);
+
+controller.init();
